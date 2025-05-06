b0a69e2efaaede7399e71f3662fee2daf35a550a
Spector-Studios <mathsandfunwithved@gmail.com>
2025-05-06 17:32:59 +0530

    trying to solve high memory usage

--- dungeoncrawl/src/main.rs
+++ dungeoncrawl/src/main.rs
@@ -11,53 +11,50 @@ mod texture_store;
 use crate::prelude::*;
 use input_lib::Controller;
 
-macro_rules! texture_from_file {
-    ($path:literal) => {
-        load_texture($path).await.expect("$path texture")
-    };
-}
-
 struct Game {
-    ecs: World,
-    systems: Schedule,
+    // ecs: World,
+    // systems: Schedule,
     controller: Controller,
 }
 
 impl Game {
     fn new(
-        sprite_sheet: SpritesStore,
-        player_texture: Rect,
-        enemy_textures: [Rect; 4],
-        floor_texture: Rect,
-        wall_texture: Rect,
+        //sprite_sheet: SpritesStore,
+        // player_texture: Rect,
+        // enemy_textures: [Rect; 4],
+        // floor_texture: Rect,
+        // wall_texture: Rect,
     ) -> Self {
-        let mut ecs = World::default();
+        // let mut ecs = World::default();
 
-        let mut rng = Rng::with_seed(macroquad::miniquad::date::now() as _);
-        let map_builder = MapBuilder::new(&mut rng, floor_texture, wall_texture);
+        // let mut rng = Rng::with_seed(macroquad::miniquad::date::now() as _);
+        // let map_builder = MapBuilder::new(&mut rng, floor_texture, wall_texture);
 
-        ecs.insert_resource(map_builder.map);
-        ecs.insert_resource(Camera::new(map_builder.player_start));
-        ecs.insert_resource(sprite_sheet);
-        ecs.insert_resource(FrameTime(0.0));
+        // ecs.insert_resource(map_builder.map);
+        // ecs.insert_resource(Camera::new(map_builder.player_start));
+        // ecs.insert_resource(sprite_sheet);
+        // ecs.insert_resource(FrameTime(0.0));
 
-        spawn_player(&mut ecs, map_builder.player_start, player_texture);
+        // spawn_player(&mut ecs, map_builder.player_start, player_texture);
 
-        for room_centre in map_builder.rooms.iter().skip(1).map(|r| r.centre()) {
-            spawn_enemy(&mut ecs, &mut rng, room_centre, enemy_textures)
-        }
+        // map_builder
+        //     .rooms
+        //     .iter()
+        //     .skip(1)
+        //     .map(|r| r.centre())
+        //     .for_each(|room_centre| spawn_enemy(&mut ecs, &mut rng, room_centre, enemy_textures));
 
         Self {
-            ecs,
-            systems: build_scheduler(),
+            // ecs,
+            // systems: build_scheduler(),
             controller: Controller::new(),
         }
     }
     fn tick(&mut self) {
         self.controller.update(); // TODO Move to ecs
-        self.ecs.insert_resource(self.controller.button_state);
+        //self.ecs.insert_resource(self.controller.button_state);
         //self.ecs.insert_resource(FrameTime(self.ecs.get_resource::<FrameTime>().unwrap().0 + get_frame_time()));
-        self.systems.run(&mut self.ecs);
+        //self.systems.run(&mut self.ecs);
 
         //draw_circle(200.0, 700.0, 90.0, VIOLET);
         //draw_texture(&self.resources.get::<TextureStore>().unwrap().map_render.texture, VIEWPORT_X, VIEWPORT_Y, WHITE);
@@ -95,29 +92,29 @@ async fn main() {
 
     let enemy_textures = [goblin, giant, twoheads, warrior]; */
 
-    let sprites = load_texture("resources/sprites.png")
-        .await
-        .expect("Sprites");
-    let sprite_sheet = SpritesStore { sprites };
+    //let sprites = load_texture("resources/sprites.png")
+    //    .await
+    //    .expect("Sprites");
+    //let sprite_sheet = SpritesStore { sprites };
 
-    let player = Rect::new(96.0, 32.0, 32.0, 32.0);
+    // let player = Rect::new(96.0, 32.0, 32.0, 32.0);
 
-    let floor_texture = Rect::new(96.0, 0.0, 32.0, 32.0);
-    let wall_texture = Rect::new(64.0, 96.0, 32.0, 32.0);
+    // let floor_texture = Rect::new(96.0, 0.0, 32.0, 32.0);
+    // let wall_texture = Rect::new(64.0, 96.0, 32.0, 32.0);
 
-    let goblin = Rect::new(32.0, 32.0, 32.0, 32.0);
-    let giant = Rect::new(0.0, 0.0, 32.0, 32.0);
-    let twoheads = Rect::new(32.0, 96.0, 32.0, 32.0);
-    let warrior = Rect::new(96.0, 96.0, 32.0, 32.0);
+    // let goblin = Rect::new(32.0, 32.0, 32.0, 32.0);
+    // let giant = Rect::new(0.0, 0.0, 32.0, 32.0);
+    // let twoheads = Rect::new(32.0, 96.0, 32.0, 32.0);
+    // let warrior = Rect::new(96.0, 96.0, 32.0, 32.0);
 
-    let enemy_textures = [goblin, giant, twoheads, warrior];
+    // let enemy_textures = [goblin, giant, twoheads, warrior];
 
     let mut game = Game::new(
-        sprite_sheet,
-        player,
-        enemy_textures,
-        floor_texture,
-        wall_texture,
+        //sprite_sheet,
+        // player,
+        // enemy_textures,
+        // floor_texture,
+        // wall_texture,
     );
     let mut frame_time = 0.0;
 
@@ -126,13 +123,13 @@ async fn main() {
         //draw_text("Hello", 100_f32, 200_f32, 12_f32, SKYBLUE);
         //game.update();
 
-        /* if frame_time > 0.1 {
+        if frame_time > (1.0/FRAME_RATE) {
             game.tick();
             //draw_rectangle(20.0, 300.0, 300.0, 400.0, WHITE);
         } else {
             frame_time += get_frame_time();
-        } */
+        }
 
-        game.tick();
+        //game.tick();
     }
 }
