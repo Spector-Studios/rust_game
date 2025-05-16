6cefba0d7b25b7407a342323959d3345de67b337
Ved Prajapati <104439483+Spector-Studios@users.noreply.github.com>
2025-05-16 21:29:04 +0530

    Added player healing when idle

--- dungeoncrawl/src/systems/player_input.rs
+++ dungeoncrawl/src/systems/player_input.rs
@@ -10,12 +10,12 @@ pub fn player_input_system(
     //map: Res<Map>,
     //mut camera: ResMut<Camera>,
     mut turn_state: ResMut<TurnState>,
-    mut player_pos_query: Query<(Entity, &TilePoint, &mut Timer), With<Player>>,
+    mut player_query: Query<(Entity, &TilePoint, &mut Health, &mut Timer), With<Player>>,
     enemy_pos_query: Query<(Entity, &TilePoint), With<Enemy>>,
     mut move_writer: EventWriter<WantsToMove>,
     mut attack_writer: EventWriter<WantsToAttack>,
 ) {
-    let (player_entity, pos, mut timer) = player_pos_query
+    let (player_entity, pos, mut health, mut timer) = player_query
         .single_mut()
         .expect("More than one or no players");
 
@@ -23,14 +23,15 @@ pub fn player_input_system(
         timer.time += get_frame_time();
     } else if *button_state != ButtonState::new() && !button_state.back {
         timer.time = 0.0;
-
-        let delta = TilePoint::new(button_state.dpad_x, -(button_state.dpad_y));
-
-        let destination = *pos + delta;
-
+        let mut did_something = false;
         let mut hit_something = false;
 
-        if destination != TilePoint::zero() {
+        let delta = TilePoint::new(button_state.dpad_x, -(button_state.dpad_y));
+        let destination = *pos + delta;
+
+        if delta != TilePoint::zero() {
+            did_something = true;
+
             enemy_pos_query
                 .iter()
                 .filter(|(_, pos)| **pos == destination)
@@ -50,6 +51,10 @@ pub fn player_input_system(
                 destination,
                 is_player: true,
             });
+        }
+
+        if !did_something {
+            health.current = i32::min(health.max, health.current + 1);
         }
         *turn_state = TurnState::PlayerTurn;
     }
