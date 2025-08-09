286a169396f6cde39388084653e209628567e827
Ved Prajapati <104439483+Spector-Studios@users.noreply.github.com>
2025-08-09 18:47:55 +0530

    Revert "Create build.gradle.kts"
    This reverts commit 700f8576486f4130bbef6256fbf694c24c50b6fb.

--- build.gradle.kts
+++
@@ -1,6 +0,0 @@
-if (hasProperty("buildScan")) {
-    extensions.findByName("buildScan")?.withGroovyBuilder {
-        setProperty("termsOfServiceUrl", "https://gradle.com/terms-of-service")
-        setProperty("termsOfServiceAgree", "yes")
-    }
-}
